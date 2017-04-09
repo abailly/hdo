325c4da6fd5f23f467d585763703755faa94b7bc
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-04-09 12:42:40 +0200

    print request body when running in debug mode

--- src/Network/REST/Conduit.hs
+++ src/Network/REST/Conduit.hs
@@ -1,28 +1,43 @@
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 -- | Implementation of @REST@ interface based on <https://haskell-lang.org/library/http-client http-conduit>
 -- standard library
 module Network.REST.Conduit where
 
-import           Control.Concurrent       (threadDelay)
+import           Control.Concurrent         (threadDelay)
 import           Control.Monad.Trans.Free
-import           Data.Aeson               (Value, decode, eitherDecode)
-import           Data.CaseInsensitive     (mk)
-import           Data.Functor             (void)
-import           Data.Text.Encoding       (encodeUtf8)
-import           Network.HTTP.Simple
+import           Data.Aeson                 (ToJSON, Value, decode,
+                                             eitherDecode, encode)
+import           Data.ByteString.Lazy.Char8 (unpack)
+import           Data.CaseInsensitive       (mk)
+import           Data.Functor               (void)
+import           Data.Text.Encoding         (encodeUtf8)
 import           Network.REST.Commands
 import           Network.URI
 
-data Debug = NoDebug
-           | Debug
+data Debug where
+  NoDebug   :: Debug
+  Debug     :: Debug
 
-debugRequest :: Debug -> Request -> IO ()
-debugRequest NoDebug _   = pure ()
-debugRequest Debug   req = putStrLn $ "request: " ++ show req
+data RequestBody where
+  Body :: forall a . (ToJSON a) => a -> RequestBody
 
-debugResponse :: (Show a) => Debug -> Response a -> IO ()
-debugResponse NoDebug _   = pure ()
-debugResponse Debug   req = putStrLn $ "response: " ++ show req
+data RequestLog where
+  RequestLog         :: Request                -> RequestLog
+  RequestLogWithBody :: Request -> RequestBody -> RequestLog
+
+debugRequest :: Debug -> RequestLog -> IO ()
+debugRequest NoDebug _                                 = pure ()
+debugRequest Debug   (RequestLog req)                  = putStrLn $ "request: " ++ show req
+debugRequest Debug   (RequestLogWithBody req (Body b)) = putStrLn $ "request: " ++ show req ++ ", body: " ++ unpack (encode b)
+
+debugResponse :: (Show a) => Debug -> IO (Response a) -> IO (Response a)
+debugResponse NoDebug act = act
+debugResponse Debug   act = do
+  response <- act
+  putStrLn $ "response: " ++ show response
+  pure response
 
 -- | An implementation of @REST@ functor based on http-client and @IO@
 runConduit :: Debug -> RESTT IO r -> IO r
@@ -42,37 +57,34 @@ runConduit debug r = do
 
       step (Free (Get uri k)) = do
         request <- parseRequest $ "GET " ++ asString uri
-        debugRequest debug request
-        response <- httpJSON request
-        debugResponse debug response
+        debugRequest debug (RequestLog request)
+        response <- debugResponse debug $ httpJSON request
         let value = getResponseBody response :: Value
-        runConduit debug (k value)
+        runConduit debug (k $ Right value)
 
       step (Free (GetWith opts uri k)) = do
         request <- parseRequest $ "GET " ++ asString uri
-        debugRequest debug request
-        response <- httpJSON $ options opts request
-        debugResponse debug response
+        debugRequest debug (RequestLog request)
+        response <- debugResponse debug $ httpJSON $ options opts request
         let value = getResponseBody response :: Value
         runConduit debug (k value)
 
       step (Free (DeleteWith opts uri k)) = do
         request <- parseRequest $ "DELETE " ++ asString uri
-        debugRequest debug request
+        debugRequest debug (RequestLog request)
         void $ httpLBS $ options opts request
         runConduit debug k
 
       step (Free (Post uri val k)) = do
         request <- parseRequest $ "POST " ++ asString uri
-        debugRequest debug request
-        response <- httpLbs $ setRequestBodyJSON val request
-        debugResponse debug response
+        debugRequest debug (RequestLogWithBody request (Body val))
+        response <- debugResponse debug  $ httpLbs $ setRequestBodyJSON val request
         let value = decode $ getResponseBody response
         runConduit debug (k value)
 
       step (Free (PostWith opts uri val k)) = do
         request <- parseRequest $ "POST " ++ asString uri
-        debugRequest debug request
+        debugRequest debug (RequestLogWithBody request (Body val))
         response <- httpLbs $ options opts $ setRequestBodyJSON val request
         debugResponse debug response
         let value :: Either String Value = eitherDecode $ getResponseBody response
